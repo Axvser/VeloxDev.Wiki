09524e85205cd6a6c84076a9e8fb3ab6037d43ee
i_fengjunjie <i_fengjunjie@cvte.com>
2025-12-24 17:41:15 +0800

    add files

---
+++ README.md
@@ -0,0 +1,3 @@
+# VeloxDev.Wiki
+
+> Powered by Avalonia... Click [me](https://axvser.github.io/VeloxDev.Wiki) to continue
---
+++ src/VeloxDev.Wiki/VeloxDev.Wiki/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VeloxDev.Wiki.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
